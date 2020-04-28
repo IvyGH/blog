3c20415edc533bb4e09f9e82d4682660fa96f0d3
root <root@DESKTOP-17CC21M.localdomain>
2020-04-28 15:06:16 +0300

    Fixed error

--- blog/blog/settings.py
+++ blog/blog/settings.py
@@ -25,7 +25,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'articles.app.ArticlesConfig'
+    'articles.apps.ArticlesConfig'
 ]
 
 MIDDLEWARE = [
@@ -109,4 +109,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.joint(BASE_DIR, 'static')
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
